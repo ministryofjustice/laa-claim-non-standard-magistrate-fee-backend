46afac24b7f64f8847d9f61ad7f026a233879b3a
David Henry <david.henry@digital.justice.gov.uk>
2023-05-10 13:42:41 +0100

    start on case disposal

--- app/services/decisions/simple_decision_tree.rb
+++ app/services/decisions/simple_decision_tree.rb
@@ -3,6 +3,16 @@ module Decisions
     def destination
       case step_name
       when :claim_type
+        after_claim_type
+      when :firm_details
+        edit(:case_disposal)
+      else
+        index('/claims')
+      end
+    end
+
+    def after_claim_type
+      if form_object.claim_type.supported?
         edit(:firm_details)
       when :firm_details
         edit(:reason_for_claim)
